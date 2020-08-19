c698963303faf0aaecdbcfcacab5ea3fe449b838
ren_hai_peng <1730172964@qq.com>
2020-08-19 20:49:31 +0800

    创建测试项目user-service,测试成功运行

---
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -0,0 +1,26 @@
+package com.kgc.kmall.user.service;
+
+import com.kgc.kmall.bean.Member;
+import com.kgc.kmall.service.MemberService;
+import com.kgc.kmall.user.mapper.MemberMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-08-19 20:44
+ */
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Resource
+    MemberMapper memberMapper;
+
+    @Override
+    public List<Member> selectAllMember() {
+        List<Member> members = memberMapper.selectByExample(null);
+        return members;
+    }
+}
