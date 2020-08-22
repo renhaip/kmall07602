d3aca331fd72d04c653f3710fcf159e4c14993ab
ren_hai_peng <1730172964@qq.com>
2020-08-22 14:51:59 +0800

    创建消费者成功,测试成功

--- kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -3,7 +3,7 @@ package com.kgc.kmall.user.service;
 import com.kgc.kmall.bean.Member;
 import com.kgc.kmall.service.MemberService;
 import com.kgc.kmall.user.mapper.MemberMapper;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
