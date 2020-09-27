bd155e8c3536b8e23aa830cde359ef56c9405460
ren_hai_peng <1730172964@qq.com>
2020-09-27 10:20:44 +0800

    搜索模块测试完成

--- kmall-search-web/src/main/java/com/kgc/kmall/kmallsearchweb/controller/SearchController.java
+++ kmall-search-web/src/main/java/com/kgc/kmall/kmallsearchweb/controller/SearchController.java
@@ -43,10 +43,27 @@ public class SearchController {
         List<PmsBaseAttrInfo> pmsBaseAttrInfos = attrService.selectAttrInfoValueListByValueId(valueIdSet);
 
 
-
-
         //已选中的valueId
         String[] valueId = pmsSearchSkuParam.getValueId();
+
+
+
+        //封装面包屑数据
+        if(valueId!=null){
+            List<PmsSearchCrumb> pmsSearchCrumbs=new ArrayList<>();
+            for (String s: valueId) {
+                PmsSearchCrumb pmsSearchCrumb=new PmsSearchCrumb();
+                pmsSearchCrumb.setUrlParam(getUrlParam(pmsSearchSkuParam,s));
+                pmsSearchCrumb.setValueId(s);
+
+                String valueName=getValueName(pmsBaseAttrInfos,s);
+                pmsSearchCrumb.setValueName(valueName);
+                pmsSearchCrumbs.add(pmsSearchCrumb);
+            }
+            model.addAttribute("attrValueSelectedList",pmsSearchCrumbs);
+        }
+
+
             //排除已选中的平台属性
         if(valueId!=null){
             //利用迭代器排除已选的平台属性,删除集合元素不能使用for循环,因为会出现数组越界
@@ -87,6 +104,54 @@ public class SearchController {
         String urlParam=buffer.substring(1);
         model.addAttribute("urlParam",urlParam);
 
+
+
+
+
+
+
+        //显示关键字
+        model.addAttribute("keyword",pmsSearchSkuParam.getKeyword());
+
         return "list";
+    }
+
+
+    //根据参数对象拼接url
+    public String  getUrlParam(PmsSearchSkuParam pmsSearchSkuParam,String valueId){
+        StringBuffer buffer=new StringBuffer();
+        String keyword = pmsSearchSkuParam.getKeyword();
+        String catalog3Id = pmsSearchSkuParam.getCatalog3Id();
+        String[] valueIds = pmsSearchSkuParam.getValueId();
+
+        if(StringUtils.isNotBlank(catalog3Id)){
+                buffer.append("&catalog3Id="+catalog3Id);
+        }
+        if(StringUtils.isNotBlank(keyword)){
+                buffer.append("&keyword="+keyword);
+        }
+        //面包屑的url是不能包括自身的valueId的，因为点击面包屑以后会从当前URL中去除面包屑的valueId
+        if(valueIds!=null){
+            for (String id : valueIds) {
+                if(valueId.equals(id)==false){
+                    buffer.append("&valueId="+id);
+                }
+            }
+        }
+        return buffer.substring(1);
+    }
+
+    //根据valueId查询valueName
+    public  String  getValueName(List<PmsBaseAttrInfo> pmsBaseAttrInfos,String valueId){
+        String valueName="";
+        for (PmsBaseAttrInfo pmsBaseAttrInfo : pmsBaseAttrInfos) {
+            for (PmsBaseAttrValue  pmsBaseAttrValue : pmsBaseAttrInfo.getAttrValueList()) {
+                    if(valueId.equals(pmsBaseAttrValue.getId().toString())){
+                       valueName=pmsBaseAttrInfo.getAttrName()+":"+pmsBaseAttrValue.getValueName();
+                        return valueName;
+                    }
+            }
+        }
+        return valueName;
     }
 }
